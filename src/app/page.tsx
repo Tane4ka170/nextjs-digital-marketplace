d82b2dd7fe41b24fb656d345be09a71c2f6d6ac6
Tane4ka170 <tane4ka170@gmail.com>
2024-11-14 16:54:48 +0200

    Improved homepage layout and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-        <h1>
+        <h1 className="text-4xl font-bold tracking-tight text-yellow-900 sm:text-6xl">
           Your marketplace for premium{" "}
           <span className="text-amber-600"> digital assets</span>
         </h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Welcome to DigitalBazaar. Each asset on our platform is reviewed by
+          our team to guarantee the highest quality standards.
+        </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href="/products" className={buttonVariants()}>
+            Explore what's trending
+          </Link>
+        </div>
       </div>
     </MaxWidthWrapper>
   );
