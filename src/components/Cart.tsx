fab928059a37cfd51467bb1d984ae4a1201dea56
Tane4ka170 <tane4ka170@gmail.com>
2024-11-19 21:11:19 +0200

    Added cart functionality with item count and total

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { Sheet, ShoppingCart } from "lucide-react";
-import { SheetTrigger } from "./ui/sheet";
+import {
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
 
 const Cart = () => {
+  const itemCount = 0;
+  const fee = 1;
+
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -11,8 +22,43 @@ const Cart = () => {
           className="h-6 w-6 flex-shrink-0 text-yellow-400 group-hover:text-yellow-500"
           aria-hidden="true"
         />
-        <span className="ml-2 text-sm font-medium text-yellow-700 group-hover:text-yellow-800"></span>
+        <span className="ml-2 text-sm font-medium text-yellow-700 group-hover:text-yellow-800">
+          0
+        </span>
       </SheetTrigger>
+      <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+        <SheetHeader className="space-y-2.5 pr-6">
+          <SheetTitle>Cart (0)</SheetTitle>
+        </SheetHeader>
+        {itemCount > 0 ? (
+          <>
+            <div className="flex w-full flex-col pr-6">
+              {/* TODO: Cart logic */}
+              cart items
+            </div>
+            <div className="space-y-4 pr-6">
+              <Separator />
+              <div className="space-y-1.5 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+              </div>
+              <SheetFooter></SheetFooter>
+            </div>
+          </>
+        ) : (
+          <div></div>
+        )}
+      </SheetContent>
     </Sheet>
   );
 };
