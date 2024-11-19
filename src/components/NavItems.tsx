37e5b71a6db847fb88df2cb309c789c8a7a18faa
Tane4ka170 <tane4ka170@gmail.com>
2024-11-19 18:25:58 +0200

    Added keyboard & click outside handling for NavItems

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,18 +1,25 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {};
+  }, []);
 
   const isAnyOpen = activeIndex !== null;
 
   const navRef = useRef<HTMLDivElement | null>(null);
 
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
