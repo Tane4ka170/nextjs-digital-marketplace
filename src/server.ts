0a099d86eac51851c6b3afa7d06b96ab4d1294c3
Tane4ka170 <tane4ka170@gmail.com>
2024-11-23 14:16:43 +0200

    Integrated Next.js with Payload CMS and Express server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { getPayloadClient } from "./get-payload";
+import { nextApp, nextHandler } from "./next-utils";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -8,8 +9,19 @@ const start = async () => {
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
-      onInit: async(cms),
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
+      },
     },
+  });
+  app.use((req, res) => nextHandler(req, res));
+
+  nextApp.prepare().then(() => {
+    payload.logger.info("Next.js started");
+
+    app.listen(PORT, async () => {
+      payload.logger.info(`Next.js app URL:${process.env.NEXT_PUBLIC_SERVICE}`);
+    });
   });
 };
 
